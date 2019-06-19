a6bdcbee64a7b3c1980ab153a3022fef413c7b7d
pixtop <pixtopmc@gmail.com>
2019-06-19 16:03:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# gatsby-source-mmf
+
+This plugin sets up source for Gatsby application fetching data from MyMiniFactory API
+
+## Install plugin
+
+To install the plugin, follow these steps.
+
+1. Gatsby loads plugins from the your local website plugins folder which is a folder named `plugins` in the website’s root directory. So place the plugin folder inside it.
+
+2. Ensure that [npm](https://www.npmjs.com/) is installed. This provides dependencies to run the plugin.
+
+3. From the plugin folder, execute the following command:
+
+  ```shell
+  npm install
+  ```
+
+## Configure the plugin
+
+Open `gatsby-config.js` from the root directory of your tutorial site, and add the gatsby-source-mmf plugin inside plugin table:
+
+```javascript
+plugins: [
+  ...,
+  {
+    resolve: "gatsby-source-mmf",
+    options: {
+      key: "<API-KEY>",
+      cat: "112",
+      per_page: "16",
+      light: "1",
+    },
+  },
+]
+```
---
+++ gatsby-node.js
@@ -0,0 +1,55 @@
+const fetch = require("node-fetch")
+const queryString = require("query-string")
+
+exports.sourceNodes = (
+	{ actions, createNodeId, createContentDigest },
+	configOptions
+) => {
+	const { createNode } = actions
+
+	// Gatsby adds a configOption that's not need for this plugin, delete it
+	delete configOptions.plugins
+	// Helper function that processes a object to match Gatsby's node structure
+	const processObject = object => {
+		const nodeId = createNodeId(`mmf-object-${object.id}`)
+		const nodeContent = JSON.stringify(object)
+		const nodeData = Object.assign({}, object, {
+			id: nodeId,
+			parent: null,
+			children: [],
+			internal: {
+				type: `MyMiniFactoryObject`,
+				content: nodeContent,
+				contentDigest: createContentDigest(object),
+			}
+		})
+		return nodeData
+	}
+
+	// Convert the options object into a query string
+	const apiOptions = queryString.stringify(configOptions)
+	// Join apiOptions with the MyMiniFactory API URL
+	const apiUrl = `https://www.myminifactory.com/api/v2/search?${apiOptions}`
+	// Gatsby expects sourceNodes to return a promise
+	return (
+		// Parse a response from the apiUrl
+		fetch(apiUrl)
+				// Parse the response as JSON
+				.then(response => {
+					if (response.ok) {
+						return response.json()
+					}
+					throw new Error('Network response was not ok.')
+				})
+				// Process the JSON data into a node
+				.then(data => {
+					// For each query result
+					data.items.forEach(item => {
+						// Process the photo data to match the structure of Gatsby node
+						const nodeData = processObject(item)
+						// Use Gatsby's createNode helper to create a node from the node data
+						createNode(nodeData)
+					})
+				}).catch( error => console.log('There has been a problem with fetch operation', error.message))
+	)
+}
